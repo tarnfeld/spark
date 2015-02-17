92752e3657f8b4b77a95ac287d2ba0f53d14729b
Chris Heller <hellertime@gmail.com>
2015-02-17 10:08:32 -0500

    Use map on Option to be consistent with non-coarse code

--- core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/CoarseMesosSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/CoarseMesosSchedulerBackend.scala
@@ -234,7 +234,7 @@ private[spark] class CoarseMesosSchedulerBackend(
             .addResources(createResource("mem",
               MemoryUtils.calculateTotalMemory(sc)))
 
-          sc.conf.getOption("spark.executor.docker.image").foreach { image =>
+          sc.conf.getOption("spark.executor.docker.image").map { image =>
             val container = task.getContainerBuilder()
             val volumes = sc.conf
               .getOption("spark.executor.docker.volumes")
